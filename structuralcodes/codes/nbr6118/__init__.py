098b990f962dc67af6a718e1f0505e37ffec4b4f
andrefmello91 <andremello91@gmail.com>
2026-01-29 17:16:51 -0400

    Added __init__.py to NBR 6118

---
+++ structuralcodes/codes/nbr6118/__init__.py
@@ -0,0 +1,28 @@
+"""ABNT NBR 6118 (2023)"""
+
+from ._concrete_material_properties import(
+  fcd,
+  fctm,
+  fctkinf,
+  fctksup,
+  Eci,
+  Ecs,
+  beta_1,
+  fckj,
+  fcdj,
+  Eci_t,
+  eta_c,
+  eps_c2,
+  eps_cu,
+  n_parabolic_rectangular,
+  fcm,
+  eps_c1,
+  eps_cu1,
+  k_sargin,
+)
+
+from ._reinforcement_material_properties import fyd
+
+__title__: str = 'ABNT NBR 6118'
+__year__: str = '2023'
+__materials__: t.Tuple[str] = ('concrete', 'reinforcement')
